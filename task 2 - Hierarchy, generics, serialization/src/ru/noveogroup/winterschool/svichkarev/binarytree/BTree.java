7083c749cb2f844f6e4fe91bb282ec3a9039a86e
Anatoly Svichkarev <tolik0393@mail.ru>
2014-03-13 19:44:29 +0700

    add remove metod to Binary Tree

--- task 2 - Hierarchy, generics, serialization/src/ru/noveogroup/winterschool/svichkarev/binarytree/BTree.java
+++ task 2 - Hierarchy, generics, serialization/src/ru/noveogroup/winterschool/svichkarev/binarytree/BTree.java
@@ -1,6 +1,7 @@
 package ru.noveogroup.winterschool.svichkarev.binarytree;
 
 import java.util.Iterator;
+import java.util.jar.JarException;
 
 import ru.noveogroup.winterschool.svichkarev.hierarhy.Parent;
 
@@ -22,8 +23,8 @@ public class BTree<T extends Parent> implements Iterable<T> {
     
     public void add( T value ) {
         //? проверка, что не null
-        Node<T> tmpNode = root; //?
-        Node<T> preNode = null; //?
+        Node<T> tmpNode = root;
+        Node<T> preNode = null;
         
         while( tmpNode != null ){
             int cmp = value.hashCode() - tmpNode.hashCode();
@@ -53,29 +54,83 @@ public class BTree<T extends Parent> implements Iterable<T> {
     }
 
     public void remove( T value ){
+      //? проверка, что не null
+        Node<T> tmpNode = root;
+        Node<T> preNode = null;
         
+        while( tmpNode != null ){
+            int cmp = value.hashCode() - tmpNode.hashCode();
+            if( cmp == 0 ){
+                break;
+            } else{
+                preNode = tmpNode;
+                if( cmp < 0 ){
+                    tmpNode = tmpNode.left;
+                } else{
+                    tmpNode = tmpNode.right;
+                }
+            }
+        }
+        // if empty tree
+        if( tmpNode == null ){
+            return;
+        }
+        
+        if( tmpNode.right == null ){
+            // if right subtree is empty
+            if( preNode == null ){
+                // if tmpNode == root
+                root = tmpNode.left;
+            } else{
+                if( tmpNode == preNode.left ){
+                    // tmpNode in left subtree of preNode
+                    preNode.left = tmpNode.left;
+                } else{
+                    // tmpNode in right subtree of preNode
+                    preNode.right = tmpNode.left;
+                }
+            }
+        } else{
+            // find in right subtree the biggest element
+            Node<T> leftMost = tmpNode.right;
+            preNode = null;
+            while( leftMost.left != null ){
+                preNode = leftMost;
+                leftMost = leftMost.left;
+            }
+            if( preNode != null ){
+                preNode.left = leftMost.right; //?
+            } else{
+                tmpNode.right = leftMost.right; //?
+            }
+            tmpNode.value = leftMost.value;
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
         Iterator<T> it = new Iterator<T>() {
-
+            private Node<T> currentNode = null;
+            
             @Override
             public boolean hasNext() {
-                // TODO Auto-generated method stub
+                // TODO
+                
                 return false;
             }
 
             @Override
             public T next() {
-                // TODO Auto-generated method stub
+                if( ! hasNext() ){
+                    throw new java.util.NoSuchElementException( "no more elements" );
+                }
+                // TODO
                 return null;
             }
 
             @Override
             public void remove() {
-                // TODO Auto-generated method stub
-                
+                throw new java.lang.UnsupportedOperationException( "remove" );
             }
         };
         
